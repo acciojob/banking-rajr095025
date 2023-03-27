dc38a4e87cfdfada91447616b851f09d1c580fc2
jaya vignesh <rajr095025@gmail.com>
2023-03-28 00:18:18 +0530

    changes

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -24,7 +24,7 @@ public class CurrentAccount extends BankAccount{
     public void setTradeLicenseId(String tradeLicenseId) {
         this.tradeLicenseId = tradeLicenseId;
     }
-
+    /*
     public void validateLicenseId() throws Exception {
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
@@ -33,16 +33,97 @@ public class CurrentAccount extends BankAccount{
         if(isValid(this.tradeLicenseId)){
             return;
         }
-
         try{
             this.tradeLicenseId = rearrangeString(this.tradeLicenseId);
         }
         catch (Exception e){
             throw e;
         }
+    }
+     */
 
+    public void validateLicenseId() throws Exception {
+        // A trade license Id is said to be valid if no two consecutive characters are same
+        // If the license Id is valid, do nothing
+        // If the characters of the license Id can be rearranged to create any valid license Id
+        // If it is not possible, throw "Valid License can not be generated" Exception
 
+        int n = tradeLicenseId.length();
+        int temp = 0;
 
+        for (int i = 0; i < n - 1; i++) {
+            if (tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i + 1))
+                break;
+            else {
+                if (tradeLicenseId.charAt(i) != tradeLicenseId.charAt(i + 1))
+                    temp++;
+            }
+        }
+        if (temp == n-1)
+            return;
+
+        int freq[] = new int[26];
+
+        for (int i = 0; i < n; i++)
+            freq[tradeLicenseId.charAt(i) - 'A']++;
+
+        int max = 0, letter = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (max < freq[i]) {
+                max = freq[i];
+                letter = i;
+            }
+        }
+
+        if (max > (n + 1) / 2)
+            throw new Exception("Valid License can not be generated");
+
+        char ch[] = new char[n];
+
+        // For Even position filling
+        for (int i = 0; i < n; i += 2)
+        {
+            if (freq[letter] > 0)
+            {
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+            else
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    if (freq[j] > 0) {
+                        letter = j;
+                        break;
+                    }
+                }
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+        }
+
+        // For Odd position filling
+        for (int i = 1; i < n; i += 2)
+        {
+            if (freq[letter] > 0)
+            {
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+            else
+            {
+                for (int j = 0; j < 26; j++) {
+                    if (freq[j] > 0) {
+                        letter = j;
+                        break;
+                    }
+                }
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+        }
+        tradeLicenseId = String.valueOf(ch);
     }
 
     public boolean isValid(String str){
@@ -115,6 +196,7 @@ public class CurrentAccount extends BankAccount{
         else
             return ansStr;
     }
+
 }
 
 class KeyComparator implements Comparator<Key> {
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         try {
-            CurrentAccount ca1 = new CurrentAccount("Rocky", 5500, "AABBA");
+            CurrentAccount ca1 = new CurrentAccount("Rocky", 5500, "AAA");
             SavingsAccount sa1 = new SavingsAccount("Rockysa1",60000,100000,2);
             System.out.println(sa1.getBalance());
             System.out.println(sa1.getSimpleInterest(3));
